60aab39badf70deae637f7f911feead453460c98
Doragon-RH <r216af@icloud.com>
2024-01-21 18:56:16 +0900

    固定のバーを表示

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,16 +1,56 @@
 // import Head from 'next/head';
+import * as React from 'react';
 import Image from 'next/image';
 import Link from "@mui/material/Link";
 import Avatar from "@mui/material/Avatar";
-import Stack from "@mui/material/Stack";
+// import Stack from "@mui/material/Stack";
 import Box from "@mui/material/Box";
 import { blue } from '@mui/material/colors';
+import AppBar from '@mui/material/AppBar';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+const drawerWidth = 1000;
+const navItems = ['Home', 'learn', 'did'];
 
 const name = 'Doragon-RH';
 export const siteTitle = 'introduction';
 export default function Layout({ children, home }) {
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        Introduction
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
     return (
-        <Box
+      <Box
         sx={{
           display: "flex",
           flexDirection: "column",
@@ -18,12 +58,58 @@ export default function Layout({ children, home }) {
           justifyContent: "center",
           bgcolor: "background.paper",
           height: "auto",
-          padding: "30px",
-          margin: "20px",
+          padding: "0px",
+          margin: "60px",
           background: "lightgreen",
           borderRadius: 40,
         }}>
-        {/* <Head>
+       <CssBaseline /> {/*//CSSのリセット */}
+      <AppBar component="nav">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            Introduction
+          </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item) => (
+              <Button key={item} sx={{ color: '#fff' }}>
+                {item}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <nav>
+        <Drawer
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+        <Toolbar />
+        
+        {/* <Head> 以前の内容
             <link rel="icon" href="/favicon.ico" />
             <meta
             name="description"
@@ -108,5 +194,6 @@ export default function Layout({ children, home }) {
           </Box>
         </Link>
       </Box>
+        
     );
 }
