42d762b54ef75dd648ecf40bb02e1e53860418eb
Doragon-RH <117128754+Doragon-RH@users.noreply.github.com>
2024-09-21 10:42:05 +0900

    Update learn.tsx

--- pages/other/learn.tsx
+++ pages/other/learn.tsx
@@ -115,7 +115,7 @@ export default function Learn() {
             This is what I learned about React!
           </Link ><br />
           <Link 
-          href="introduction-12amg1mg7-honbinryus-projects.vercel.app" 
+          href="introduction-liard.vercel.app" 
             
           underline="hover">
             This is what I learned about Next.js!
